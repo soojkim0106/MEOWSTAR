7c80e1e22bf515531709979ba5b8ac85d4a8aa33
Soojungkim <soojung.kimm@gmail.com>
2024-04-16 09:11:48 -0700

    login/logout added

--- client/src/components/cat/FosterAdoptForm.js
+++ client/src/components/cat/FosterAdoptForm.js
@@ -1,12 +1,18 @@
-import { useEffect } from 'react'
+import {object, string, number} from 'yup'
+import { Formik } from 'formik'
+import React, {useState, useEffect} from 'react'
 import { useNavigate, useOutletContext } from 'react-router-dom'
-import toast, {Toaster} from 'react-hot-toast'
+import toast from 'react-hot-toast'
 
 const FosterAdoptForm = () => {
 
-  const {updateCurrentUser} = useOutletContext()
+  const {currentUser, updateCurrentUser, addAdoptFosterForm} = useOutletContext()
   const navigate = useNavigate()
 
+  const fosterAdoptSchema = object({
+    catName: string().required(),
+    adoptionFee: number().min(0).max(250).required()
+  })
 
     useEffect(() => {
     fetch("/me")
@@ -22,7 +28,53 @@ const FosterAdoptForm = () => {
   }, []);
 
   return (
-    <div>FosterAdoptForm</div>
+    <div>
+      <Formik
+          initialValues={{catName: '', adoptionFee: ''}}
+          validationSchema={fosterAdoptSchema}
+          onSubmit={(values) => {
+            fetch("/adoptfoster", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({ ...values})
+            })
+            .then(resp => {
+              if (resp.status === 201) {
+                resp.json().then(createdForm => {
+                  addAdoptFosterForm(createdForm)
+                  return createdForm
+                }).then(createdForm => navigate(`/adoptfoster/${createdForm.id}`))
+              } else {
+                return resp.json().then(errorObj => toast.error(errorObj.message))
+              }
+            })
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <label>Cat Name </label>
+              <input type='text' name='catName' onChange={handleChange} onBlur={handleBlur} value={values.catName}/>
+              {errors.title && touched.title && <div className='error-message show'>{errors.title}</div>}
+
+              <label> Adoption Fee</label>
+              <input type='text' name='adoptionFee' onChange={handleChange} onBlur={handleBlur} value={values.AdoptionFee}/>
+              {errors.genre && touched.genre && <div className='error-message show'>{errors.genre}</div>}
+
+              <input type='submit' disabled={isSubmitting} />
+            </form>
+          )}
+        </Formik> 
+    </div>
   )
 }
 
--- client/src/components/navigation/Header.js
+++ client/src/components/navigation/Header.js
@@ -1,17 +1,42 @@
-import { Toaster } from 'react-hot-toast'
-import { NavLink, Link } from "react-router-dom";
+import toast, { Toaster } from 'react-hot-toast'
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useState } from "react"
 
-const Header = () => {
+const Header = ({currentUser, updateCurrentUser}) => {
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    fetch('/logout', {method:'DELETE'})
+    .then(() => {
+      updateCurrentUser(null)
+      toast.success("Logged out!")
+      navigate("/")
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <>
     <Toaster />
     <nav className="navbar">
 
       <NavLink to='/'>Home</NavLink> <br></br>
-      <NavLink to='/users/:userId'>Profile</NavLink> <br></br>
-      <NavLink to='/registration'>Sign Up/Login</NavLink> <br></br>
-      <NavLink to='/fosteradopt/new'>Adopt/Foster</NavLink>
+      <>
+        {currentUser ? (
+          <div className="container">
+            <Link to={`/users/${currentUser.id}`}>
+              Profile
+            </Link> <br></br>
+            <p onClick={handleLogout}>Logout</p>
+          </div> 
+        ) : (
+          <Link to={"/registration"}>
+            Login
+          </Link>
+        )}
+      </>
+      <NavLink to='/fosteradopt/new'>Adopt/Foster</NavLink><br></br>
     </nav>
     </>
   )
