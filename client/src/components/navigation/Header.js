2712db309ff89797b82f0b249c9be03ce48699cf
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-04-17 14:15:17 -0700

    correct header included

--- client/src/components/navigation/Header.js
+++ client/src/components/navigation/Header.js
@@ -1,48 +1,27 @@
-import toast, { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from "react-hot-toast";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import { useState } from "react"
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
+import { useState } from "react";
 
-const Header = ({currentUser, updateCurrentUser}) => {
-
-  const navigate = useNavigate()
-
-
-  const handleLogout = () => {
-    fetch('/logout', {method:'DELETE'})
-    .then(() => {
-      updateCurrentUser(null)
-      toast.success("Logged out!")
-      navigate("/")
-    })
-    .catch(err => console.log(err))
-  }
-
+const Header = ({ currentUser, handleLogout }) => {
   return (
     <>
-    <Toaster />
-    <nav className="navbar">
-
-      <NavLink to='/'>Home</NavLink> <br></br>
-      <>
-        {currentUser ? (
-          <div className="container">
-            <NavLink to={`/users/${currentUser.id}`}>
-              Profile
-            </NavLink> <br></br>
-            <NavLink onClick={handleLogout}>Logout</NavLink>
-          </div> 
-        ) : (
-          <Link to={"/registration"}>
-            Login / Sign up
-          </Link>
-        )}
-      </>
-      <NavLink to='/fosteradopt/new'>Adopt/Foster</NavLink><br></br>
-    </nav>
+      <Toaster />
+      <nav className="navbar">
+        <NavLink to="/">Home</NavLink> <br></br>
+        <>
+          {currentUser ? (
+            <div className="container">
+              <NavLink to={`/users/${currentUser.id}`}>Profile</NavLink>{" "}
+              <br></br>
+              <NavLink onClick={handleLogout}>Logout</NavLink>
+            </div>
+          ) : (
+            <Link to={"/registration"}>Login / Sign up</Link>
+          )}
+        </>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
